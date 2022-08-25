823a627f690b96403ce21f115ecfcb6d6b127cb9
Guillaume Penaud <guillaume.penaud@gmail.com>
2022-08-25 16:30:04 +0200

    Add variables to system.tf

--- variables.tf
+++ variables.tf
@@ -1,3 +1,6 @@
+#
+# global variables
+# ----------------
 
 variable project_id {
   type        = string
@@ -8,8 +11,46 @@ variable project_id {
   // default     = "341d39c7-1613-45d9-8b9f-e3c001c46cb0"
 }
 
+variable domain {
+  type        = string
+  description = "the dns domain used by ingress hostnames"
+  default     = "leportail.org"
+}
+
 variable deploy_monitoring_stack {
-  description = "Determine wether or not we should deploy the monitoring stack (prometheus & grafana & loki)"
   type        = bool
+  description = "Determine wether or not we should deploy the monitoring stack (prometheus & grafana & loki)"
   default     = "false"
+}
+
+variable certificate_issuer {
+  type        = string
+  description = "Set the correct certificate issuer for ingress cert-manager"
+  default     = "letsencrypt-staging-clusterissuer" // for production certificate: "letsencrypt-production-clusterissuer"
+}
+
+#
+# grafana specific variables
+# --------------------------
+
+variable grafana_ingress_hostname {
+  type        = string
+  description = "ingress hostname used by grafana server"
+  default     = "test-monitoring"
+}
+
+#
+# prometheus specific variables
+# -----------------------------
+
+variable prometheus_server_ingress_hostname {
+  type        = string
+  description = "ingress hostname used by prometheus server"
+  default     = "test-monitoring-prometheus"
+}
+
+variable prometheus_alertmanager_ingress_hostname {
+  type        = string
+  description = "ingress hostname used by prometheus alertmanager"
+  default     = "test-monitoring-alertmanager"
 }
