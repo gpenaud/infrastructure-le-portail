d0cd0c48434a837e831adb95c3d2353f419fbc1c
Guillaume Penaud <guillaume.penaud@gmail.com>
2022-08-28 22:35:53 +0200

    Add variables in application module

---
+++ layers/application/variables.tf
@@ -0,0 +1,9 @@
+#
+# global variables
+# ----------------
+
+variable force_helm_upgrade {
+  type        = bool
+  description = "wether or not force helm release to be upgraded"
+  default     = "false"
+}
